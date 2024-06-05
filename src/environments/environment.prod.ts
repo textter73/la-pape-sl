89b78dae2a9dbd301a0aa1b561a04b8ede228cc4
Angel Ortiz <jortiz@fmf.mx>
2024-06-04 20:46:10 -0600

    git commit initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBLL4XVze3DJ_GL_jayvmAzTPAs8fdcI8w",
+    authDomain: "la-pape-sl.firebaseapp.com",
+    projectId: "la-pape-sl",
+    storageBucket: "la-pape-sl.appspot.com",
+    messagingSenderId: "852384889219",
+    appId: "1:852384889219:web:ce2a3988b9c0357ae4deb6",
+    measurementId: "G-ZTXLHXFDWP"
+  }
+};
